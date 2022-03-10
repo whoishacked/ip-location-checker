3715215da8e4644b23f53e18940b3678c729e153
Andrew Kutuzov <britvill@gmail.com>
2022-03-10 20:31:34 +0300

    First version

---
+++ checker.py
@@ -0,0 +1,17 @@
+import json
+import urllib.request
+
+with open('ip_list.txt', 'r') as infile:
+    data = infile.readlines()
+    for ip in data:
+        ip_list = ip.split()
+
+for ip in ip_list:
+    response = urllib.request.urlopen("http://ipwhois.app/json/" + ip)
+    geolocation = json.load(response)
+    print(f'{geolocation["ip"]} : {geolocation["country"]}')
+
+
+
+
+
